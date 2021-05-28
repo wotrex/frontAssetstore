53815c2d81a5ccbe8b976b90c17cd84064615c20
name <steepcraftt@gmail>
2021-05-28 19:12:09 +0300

    newHeroku3

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static('./dist/frontAssetstore'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/frontAssetstore/'}),
+    res.sendFile('index.html', {root: 'dist/frontAssetstore/src/app/'}),
 );
 
 // Start the app by listening on the default Heroku port
