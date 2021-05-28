05bc1b77e10b07a28b2ee17af2ee9b67bc1fda9e
name <steepcraftt@gmail>
2021-05-28 19:45:11 +0300

    newHeroku4

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/frontAssetstore'));
+app.use(express.static('./dist/my-app'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'frontAssetstore/src/app/'}),
+    res.sendFile('index.html', {root: 'dist/my-app'}),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080);
